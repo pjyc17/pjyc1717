2aa1cbfb821e077a1f0fc2110fa96903c3152d84
pjyc17 <pjyc17@gmail.com>
2022-04-12 00:57:59 +0900

    change the code

--- 22.04.11/boj_1978_소수찾기.py
+++ 22.04.11/boj_1978_소수찾기.py
@@ -1,13 +1,14 @@
 n = int(input())
 lst = list(map(int, input().split()))
-print(lst)
-cnt = 0
 ans = 0
-print(int(lst[0]**(0.5))+1)
 for i in range(n):
-    for j in range(2, int(lst[i]**(0.5))+1):
+    cnt = 0
+    if lst[i] == 1:
+        continue
+    for j in range(2, int(lst[i])+1):
         if lst[i]%j == 0:
             cnt += 1
-    if cnt >= 2:
+    if cnt == 1:
         ans += 1
+    
 print(ans)

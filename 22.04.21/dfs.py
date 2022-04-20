9baf39488e8f6cae5d68e6bb9a62ea0af36de755
pjyc17 <pjyc17@gmail.com>
2022-04-21 06:19:30 +0900

    rebfsdfs

---
+++ 22.04.21/dfs.py
@@ -0,0 +1,18 @@
+from collections import deque
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=" ")
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[v]:
+                queue.append(i)
+                visited[v] = True
+

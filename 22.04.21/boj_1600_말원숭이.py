ad515b2f8504d6735a9b9ada8fd2d9bc47aa5854
pjyc17 <pjyc17@gmail.com>
2022-04-22 03:28:12 +0900

    horse...1600

--- 22.04.21/boj_1600_말원숭이.py
+++ 22.04.21/boj_1600_말원숭이.py
@@ -1,32 +1,37 @@
-from sys import stdin, stdout
+from sys import stdin
 input = stdin.readline
-print = stdout.write
 K = int(input())
 W, H = map(int, input().split())
-board = [map(int, input().split()) for _ in range(H)]
-
+board = [list(map(int, input().split())) for _ in range(H)]
 dr = [1, 0, -1, 0]
 dc = [0, 1, 0, -1]
 
 hr = [2, 2, 1, -1, -2, -2, -1, 1]
 hc = [1, -1, -2, -2, -1, 1, 2, 2]
 
-visited = [[0] * W for _ in range(H)]
+visited = [[[0] * W for _ in range(H)] for __ in range(K+1)]
 
-def dfs(x, y, cnt):
-    visited[x][y] = 1
+from collections import deque
+q = deque()
+q.append([0,0,0,0])
+visited[0][0][0] = 1
+ans = -1
+
+while q:
+    x, y, cnt, horse = q.popleft()
+    if x == H - 1 and y == W -1:
+        ans = cnt
+        break
     for i in range(4):
         nr, nc = x + dr[i], y + dc[i]
-        if 0 <= nr < H and 0 <= nc < W and visited[nr][nc] == 0:
-            visited[nr][nc] = 1
-            cnt = dfs(nr, nc, cnt+1)
-    return cnt
+        if 0 <= nr < H and 0 <= nc < W and board[nr][nc] != 1 and visited[horse][nr][nc] == 0:
+            visited[horse][nr][nc] = 1
+            q.append([nr, nc, cnt+1, horse])
+    if (horse < K):
+        for i in range(8):
+            nr, nc = x + hr[i], y + hc[i]
+            if 0 <= nr < H and 0 <= nc < W and board[nr][nc] != 1 and visited[horse+1][nr][nc] == 0:
+                visited[horse+1][nr][nc] = 1
+                q.append([nr, nc, cnt+1, horse+1])
 
-for i in range(H):
-    for j in range(W):
-        if board[i][j] == 0 and visited[i][j] == 0:
-            dfs(i,j,1)
-
-            
-
-def dfs_h(x, y, k)
+print(ans)
--- 22.04.21/boj_3190_뱀.py
+++ 22.04.21/boj_3190_뱀.py
@@ -2,6 +2,28 @@ dir = {0 : (1,0), 1 : (0,-1), 2 : (-1,0), 3 : (0,1)}
 
 N = int(input())
 applePoint = int(input())
-graph = [list(map(int, input().split()))]
+graph = [[0] * N for _ in range(N)]
+
 for i in range(applePoint):
     x, y = map(int, input().split())
+    graph[x][y] = -1
+
+L = int(input())
+time_to_do = []
+for _ in range(L):
+    X, C = map(str, input().split())
+    X = int(X)
+    time_to_do.append((X,C))
+
+graph[0][0] = 1
+while True:
+    for i in range(4):
+        nr = x + dr[i]
+    if 0 > nr or nr >= N or 0 > nc or nc >= N or graph[nr][nc] == 1:
+        
+        break
+for i in range(time_to_do[0][0]+1):
+    x += dir[3][0]; y += dir[3][1]
+    if graph[x][y] == -1:
+        graph[x-dir[3][0]][y-dir[3][1]] = 1
+
